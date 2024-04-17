27d57f72b07ef9cdfb15d61c82a51a89ae8b208b
csuftitan <csuftitan@MacBook-Pro-10.local>
2024-04-16 20:49:18 -0700

    Please for the love of god

---
+++ WellNest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WellNest
+//
+//  Created by csuftitan on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("I live in a constant state of agony")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WellNest/WellNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellNestApp.swift
+//  WellNest
+//
+//  Created by csuftitan on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WellNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
