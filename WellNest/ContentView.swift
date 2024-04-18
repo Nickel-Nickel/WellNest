1a28d7bf012adb7296859b2560eed4a5453e8916
beestrubz <brandonstruble@ymail.com>
2024-04-17 20:05:55 -0700

    TabView added

--- WellNest/ContentView.swift
+++ WellNest/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        /* TabView is a SwiftUI container view used to
+        display multiple views as tabs */
         TabView {
-            HomeView()
-                .tabItem{
+            HomeView().tabItem{
+                    //SF Symbols are used for the tab's icon
                     Image(systemName: "house")
                     Text("Home")
                 }
