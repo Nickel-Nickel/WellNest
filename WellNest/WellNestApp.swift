8afa2217c11f7606d94416237108d420c60d4df7
beestrubz <brandonstruble@ymail.com>
2024-04-19 15:49:27 -0700

    MVC Folders added
    We have added Model, View, & Controller folders to organize the project and follow a standard  architectural pattern

--- WellNest/WellNestApp.swift
+++ WellNest/WellNestApp.swift
@@ -5,6 +5,10 @@
 //  Created by csuftitan on 4/16/24.
 //
 
+/* this file serves as like the "AppDelegate" thats in
+the UIKit framework where this is the entry point of our
+ application*/
+
 import SwiftUI
 
 @main
