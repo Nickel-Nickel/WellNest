5157d429d8cb98918a4fb08bac37988aa9cf2113
beestrubz <144204570+beestrubz@users.noreply.github.com>
2024-05-16 13:34:38 -0700

    Update WorkoutView.swift
    Added the basic layout for WorkoutView. There is an Add Workout button that doesn't do anything at the moment. The goal is to then prompt the user to enter the title for workout (ex: push day), then enter the number of sets &  lbs for the specific lifts.

--- WellNest/Views/WorkoutView.swift
+++ WellNest/Views/WorkoutView.swift
@@ -9,10 +9,55 @@ import SwiftUI
 
 struct WorkoutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: 0.98, green: 0.95, blue: 0.95)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Your Workouts")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.black)
+                    .padding()
+                    .padding(.trailing, 120)
+                Spacer()
+            }
+            
+            VStack {
+                Spacer() // push the button to the bottom
+                HStack {
+                    Spacer() // push the button to the right
+                    AddWorkoutButton(title: "Add Workout", textColor: .white, backgroundColor: .blue)
+                        .padding(.bottom, 50) // Adjust the padding value to move the button higher
+                        .padding(.trailing, 20) // Adjust padding to the right if needed
+                }
+            }
+        }
+    }
+}
+
+struct AddWorkoutButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Button(action: {
+            // action when pressed
+            print("Button tapped")
+        }) {
+            Text(title)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .foregroundColor(textColor) // Set the text color to white
+                .frame(width: 150, height: 50)
+                .background(backgroundColor)
+                .cornerRadius(5.0)
+        }
     }
 }
 
 #Preview {
     WorkoutView()
 }
+
+
+
