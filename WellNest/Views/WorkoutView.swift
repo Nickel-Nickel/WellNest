e5ae6f6f0593599de4206e624b98558252e82a58
beestrubz <144204570+beestrubz@users.noreply.github.com>
2024-05-17 11:22:15 -0700

    Update WorkoutView.swift
    Now has a sheet animation

--- WellNest/Views/WorkoutView.swift
+++ WellNest/Views/WorkoutView.swift
@@ -5,14 +5,19 @@
 //  Created by Reyssan Sambitan on 4/18/24.
 //
 
+// a good video reference can be found on YouTube by Sean Allen
+// on how stacks work
+
 import SwiftUI
 
 struct WorkoutView: View {
+    @State private var showingWorkoutInput = false
+
     var body: some View {
         ZStack {
-            Color(red: 0.98, green: 0.95, blue: 0.95)
+            Color.white
                 .edgesIgnoringSafeArea(.all)
-            
+
             VStack {
                 Text("Your Workouts")
                     .font(.system(size: 32, weight: .medium, design: .default))
@@ -21,14 +26,26 @@ struct WorkoutView: View {
                     .padding(.trailing, 120)
                 Spacer()
             }
-            
+
             VStack {
-                Spacer() // push the button to the bottom
+                Spacer()
                 HStack {
-                    Spacer() // push the button to the right
-                    AddWorkoutButton(title: "Add Workout", textColor: .white, backgroundColor: .blue)
-                        .padding(.bottom, 50) // Adjust the padding value to move the button higher
-                        .padding(.trailing, 20) // Adjust padding to the right if needed
+                    Spacer()
+
+                    Button(action: {
+                        showingWorkoutInput = true
+                    }) {
+                        AddWorkoutButton(title: "Add Workout", textColor: .white, backgroundColor: .blue)
+                    }
+                    .padding(.bottom, 50)
+                    .padding(.trailing, 20)
+                    /* the sheet modifier will "slide up" a view/segue
+                     where the user can enter info*/
+                    // only if the sheet is present will I allow for input
+                    .sheet(isPresented: $showingWorkoutInput) {
+                        // control is passed to the WorkoutInputView file
+                        WorkoutInputView()
+                    }
                 }
             }
         }
@@ -39,25 +56,21 @@ struct AddWorkoutButton: View {
     var title: String
     var textColor: Color
     var backgroundColor: Color
-    
+
     var body: some View {
-        Button(action: {
-            // action when pressed
-            print("Button tapped")
-        }) {
-            Text(title)
-                .font(.system(size: 20, weight: .bold, design: .default))
-                .foregroundColor(textColor) // Set the text color to white
-                .frame(width: 150, height: 50)
-                .background(backgroundColor)
-                .cornerRadius(5.0)
-        }
+        Text(title)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .foregroundColor(textColor)
+            .frame(width: 150, height: 50)
+            .background(backgroundColor)
+            .cornerRadius(5.0)
     }
 }
 
 #Preview {
     WorkoutView()
 }
+
 
 
 
